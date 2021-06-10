7c453d95bb2c40bf342f6a44f12040617f6dcd7f
Danish Shafi <danish.shafi@live.co.uk>
2021-06-10 18:13:06 +0100

    Updated

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,20 @@
+// Values & Variables
+
+const country = 'UK';
+const continent = 'Europe';
+const population = 65000000;
+
+console.log(country + continent + population);
+
+// Data Types
+
+const isIsland = false;
+
+// Operator Precedence
+
+let x;
+let y;
+
+x = y = 25 - 10 - 5;
+
+console.log(x, y);
