0fb98c457ca43327efe594fddc0d610d0f39a5b1
Danish Shafi <danish.shafi@live.co.uk>
2021-06-17 18:15:51 +0100

    Coding Challenge 1 complete

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -18,3 +18,17 @@ let y;
 x = y = 25 - 10 - 5;
 
 console.log(x, y);
+
+// Coding Challenge
+
+const markHeight = 1.88;
+const markWeight = 90;
+const johnHeight = 1.76;
+const johnWeight = 100;
+
+const markBMI = markWeight / markHeight ** 2;
+const johnBMI = johnWeight / johnHeight ** 2;
+
+const markHigherBMI = markBMI > johnBMI;
+
+console.log(markHigherBMI);
