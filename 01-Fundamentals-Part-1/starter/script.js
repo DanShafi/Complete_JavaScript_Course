697a3e6b46824928d755c0a804f9259d42eaff9b
Danish Shafi <danish.shafi@live.co.uk>
2021-06-23 18:23:36 +0100

    Code Challenge 2 complete

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -32,3 +32,37 @@ const johnBMI = johnWeight / johnHeight ** 2;
 const markHigherBMI = markBMI > johnBMI;
 
 console.log(markHigherBMI);
+
+// Template Literals
+
+const firstName = 'Dan';
+const age = 28;
+const job = 'Software Engineer';
+const year = 2021;
+
+const aboutMe = `Hello, my name is ${firstName} and I am ${age}. I am currently training
+to become a ${job} by the end of ${year}!`;
+
+console.log(aboutMe);
+
+// Drivers License Challenge
+
+const ageChecker = driverAge => {
+  if (driverAge < 18) {
+    const yearsLeft = 18 - driverAge;
+    return `Sorry, you cannot drive yet. You have to wait another ${yearsLeft} years.`;
+  }
+  return 'You can drive! :)';
+};
+
+// Coding Challenge 2
+
+if (markBMI > johnBMI) {
+  console.log(
+    `Mark's BMI (${markBMI}) is higher than John's BMI (${johnBMI}).`
+  );
+} else {
+  console.log(
+    `Johns's BMI (${johnBMI}) is higher than Mark's BMI (${markBMI}).`
+  );
+}
