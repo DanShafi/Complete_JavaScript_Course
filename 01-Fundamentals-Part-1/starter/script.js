a2de137b396998da3f2fc78565d1a7af7d1613e1
Danish Shafi <danish.shafi@live.co.uk>
2021-06-28 16:20:40 +0100

    Switch statements

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,99 +1,130 @@
-// Values & Variables
+// // Values & Variables
 
-const country = 'UK';
-const continent = 'Europe';
-const population = 65000000;
+// const country = 'UK';
+// const continent = 'Europe';
+// const population = 65000000;
 
-console.log(country + continent + population);
+// console.log(country + continent + population);
 
-// Data Types
+// // Data Types
 
-const isIsland = false;
+// const isIsland = false;
 
-// Operator Precedence
+// // Operator Precedence
 
-let x;
-let y;
+// let x;
+// let y;
 
-x = y = 25 - 10 - 5;
+// x = y = 25 - 10 - 5;
 
-console.log(x, y);
+// console.log(x, y);
 
-// Coding Challenge
+// // Coding Challenge
 
-const markHeight = 1.88;
-const markWeight = 90;
-const johnHeight = 1.76;
-const johnWeight = 100;
+// const markHeight = 1.88;
+// const markWeight = 90;
+// const johnHeight = 1.76;
+// const johnWeight = 100;
 
-const markBMI = markWeight / markHeight ** 2;
-const johnBMI = johnWeight / johnHeight ** 2;
+// const markBMI = markWeight / markHeight ** 2;
+// const johnBMI = johnWeight / johnHeight ** 2;
 
-const markHigherBMI = markBMI > johnBMI;
+// const markHigherBMI = markBMI > johnBMI;
 
-console.log(markHigherBMI);
+// console.log(markHigherBMI);
 
-// Template Literals
+// // Template Literals
 
-const firstName = 'Dan';
-const age = 28;
-const job = 'Software Engineer';
-const year = 2021;
+// const firstName = 'Dan';
+// const age = 28;
+// const job = 'Software Engineer';
+// const year = 2021;
 
-const aboutMe = `Hello, my name is ${firstName} and I am ${age}. I am currently training
-to become a ${job} by the end of ${year}!`;
+// const aboutMe = `Hello, my name is ${firstName} and I am ${age}. I am currently training
+// to become a ${job} by the end of ${year}!`;
 
-console.log(aboutMe);
+// console.log(aboutMe);
 
-// Drivers License Challenge
+// // Drivers License Challenge
 
-const ageChecker = driverAge => {
-  if (driverAge < 18) {
-    const yearsLeft = 18 - driverAge;
-    return `Sorry, you cannot drive yet. You have to wait another ${yearsLeft} years.`;
-  }
-  return 'You can drive! :)';
-};
+// const ageChecker = driverAge => {
+//   if (driverAge < 18) {
+//     const yearsLeft = 18 - driverAge;
+//     return `Sorry, you cannot drive yet. You have to wait another ${yearsLeft} years.`;
+//   }
+//   return 'You can drive! :)';
+// };
 
-// Coding Challenge 2
+// // Coding Challenge 2
 
-if (markBMI > johnBMI) {
-  console.log(
-    `Mark's BMI (${markBMI}) is higher than John's BMI (${johnBMI}).`
-  );
-} else {
-  console.log(
-    `Johns's BMI (${johnBMI}) is higher than Mark's BMI (${markBMI}).`
-  );
-}
+// if (markBMI > johnBMI) {
+//   console.log(
+//     `Mark's BMI (${markBMI}) is higher than John's BMI (${johnBMI}).`
+//   );
+// } else {
+//   console.log(
+//     `Johns's BMI (${johnBMI}) is higher than Mark's BMI (${markBMI}).`
+//   );
+// }
 
-// Type conversion on a string
+// // Type conversion on a string
 
-const inputYear = '1992';
-const inputYearAsNum = Number(inputYear);
-console.log(inputYear, inputYearAsNum);
+// const inputYear = '1992';
+// const inputYearAsNum = Number(inputYear);
+// console.log(inputYear, inputYearAsNum);
 
-const numAsNum = 1;
-const numAsString = String(numAsNum);
-console.log(numAsNum, numAsString);
+// const numAsNum = 1;
+// const numAsString = String(numAsNum);
+// console.log(numAsNum, numAsString);
 
-// 5 falsy numbers in JS are: 0, '', undefined, null, NaN
+// // 5 falsy numbers in JS are: 0, '', undefined, null, NaN
 
-const money = 0;
-if (money) {
-  console.log('This is a truthy value!');
-} else {
-  console.log('This is a falsy value!');
-}
+// const money = 0;
+// if (money) {
+//   console.log('This is a truthy value!');
+// } else {
+//   console.log('This is a falsy value!');
+// }
 
-// === does not use type coercion, == does.
+// // === does not use type coercion, == does.
 
-const favourite = prompt('What is your favourite number?');
+// const favourite = prompt('What is your favourite number?');
 
-if (favourite == 23) {
-  console.log('The input was type coerced.');
-} else if (favourite === '23') {
-  console.log('The input is strict.');
-} else {
-  console.log('Nothing to see here.');
+// if (favourite == 23) {
+//   console.log('The input was type coerced.');
+// } else if (favourite === '23') {
+//   console.log('The input is strict.');
+// } else {
+//   console.log('Nothing to see here.');
+// }
+
+// Code Challenge 3
+
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88, 91, 110) / 3;
+
+// if (scoreDolphins > scoreKoalas) {
+//   console.log('The Dolphins have won!');
+// } else if (scoreKoalas > scoreDolphins) {
+//   console.log('The Koalas have won!');
+// } else {
+//   console.log("It's a draw!");
+// }
+
+// SWITCH STATEMENT
+
+const day = prompt('What day would you like to see?');
+
+switch (day) {
+  case 'monday':
+    console.log('You are working 8-9');
+    break;
+  case 'tuesday':
+    console.log('You are working 8-8');
+    break;
+  case 'wednesday':
+    console.log('You have a day off!');
+    break;
+  default:
+    console.log('Invalid selection');
 }
