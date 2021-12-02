12d00bf0552d977d58f9c86fb66eafb7478d0a15
Danish Shafi <danish.shafi@live.co.uk>
2021-12-02 17:27:20 +0000

    Coding Challenge 2 complete

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,4 @@
-// CODING CHALLENGE 1
-
+/* CODING CHALLENGE 1
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
 const dolphinScore = calcAverage(85, 54, 41);
@@ -16,3 +15,22 @@ const checkWinner = (avgDolphins, avgKoalas) => {
 };
 
 checkWinner(dolphinScore, koalasScore);
+*/
+
+// const friends = ['Amy', 'Simon', 'George'];
+// friends.push('Dan');
+// console.log(friends);
+// friends.unshift('John');
+// console.log(friends);
+
+// CODING CHALLENGE 2
+
+const calcTip = billValue =>
+  billValue >= 50 && billValue <= 300 ? billValue * 0.15 : billValue * 0.2;
+
+const bills = [122, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(total);
