2473e3061bdec417cbf82c8633cf9e5f876dc6b6
Danish Shafi <danish.shafi@live.co.uk>
2021-12-08 16:46:07 +0000

    Modal Challenge complete

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const modalOne = document.querySelector('.show-modal');
+
+const modalToggle = () => {
+    document.querySelectorAll('div').forEach(div =>
+        div.classList.toggle("hidden")
+)};
+
+modalOne.addEventListener("click", () => {
+    modalToggle();
+});
+
+document.addEventListener("keydown", ({key}) => {
+    if (key === "Escape") {
+    modalToggle();
+    }
+});
